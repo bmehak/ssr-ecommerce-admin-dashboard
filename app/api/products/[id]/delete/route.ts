92dea9aca3ca8d4ea6e59ec85383580c9c0e8141
Bhoomika Chourasiya <bhoomikachourasiya84@gmail.com>
2025-12-27 13:21:03 +0530

    Fix delete route typing for Next.js App Router

--- app/api/products/[id]/delete/route.ts
+++ app/api/products/[id]/delete/route.ts
@@ -2,12 +2,8 @@ import { NextResponse } from "next/server";
 import { connectDB } from "../../../../../lib/db";
 import { Product } from "../../../../../models/Product";
 
-export async function POST(req: Request, { params }) {
+export async function POST(req: Request, context: { params: { id: string} }) {
   await connectDB();
-
-  await Product.findByIdAndDelete(params.id);
-
-  return NextResponse.redirect(
-    new URL("/dashboard/products", req.url)
-  );
+  await Product.findByIdAndDelete(context.params.id);
+  return NextResponse.redirect(new URL("/dashboard/products", req.url));
 }
